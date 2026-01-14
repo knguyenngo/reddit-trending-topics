4b670e93092e18afc8d8e0ef8aedc0b266b320c1
knguyenngo <khuongnguyenngo@gmail.com>
2026-01-13 23:17:21 -0500

    Removed analyse_data.py

--- src/pipeline/preprocessing.py
+++ src/pipeline/preprocessing.py
@@ -5,14 +5,25 @@ def clean(text):
     text = text.lower()
     text = re.sub(r'\d+', '', text) # Remove numbers
     text = re.sub(r'http\S+', '', text) # Remove links
-    text = re.sub(r'\W', ' ', text) # Remove special chars
+    text = re.sub(r'(?![A-Za-z]\W[A-Za-z])\W', ' ', text) # Remove special chars
 
-#def tokenize(text):
+    return text
 
+def tokenize(text):
+    tokens = text.split()
+    print(tokens)
 #def remove_stopwords(tokens, stopwords):
 
 #def load_stopwords():
 
 #def preprocess_comment(text, stopwords):
 
+def main():
+    with open("../data/raw/post_comments/1q8inb4_2026-01-12_1768238336.0916393.json", mode="r", encoding="utf-8") as read_json:
+        texts = json.load(read_json)
+    for t in texts[:2]:
+        t = clean(t)
+        tokenize(t)
 
+if __name__ == "__main__":
+    main()
